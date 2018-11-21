235d56301fef0902223e4acd17fe8ab367e2375e
Pierre Ortega <pierreandreisortega@gmail.com>
2018-11-20 20:59:06 -0800

    fix/unused deps

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 // next.config.js
 const withTypescript = require("@zeit/next-typescript");
 module.exports = {
-  ...withTypescript({
-    webpack: config => {
-      config.module.rules.push({
-        test: /\.md$/,
-        use: "raw-loader",
-      });
-
-      return config;
-    },
-  }),
+  ...withTypescript(),
   publicRuntimeConfig: {
     //api: process.env.API_HOST || "http://localhost:8080",
     api: process.env.API_HOST || "https://services.vgpro.gg/",
