0e9ec58cc2a8d45ad74ffec1aeef26a067e7827c
Pierre Ortega <pierreandreisortega@gmail.com>
2018-12-01 20:14:12 -0800

    fix/ rules

---
+++ components/md/faqContent.ts
@@ -0,0 +1,55 @@
+export default `
+# Frequently Asked Questions (FAQ)
+
+### I can not make PRIME hours. Are there any other options for me?
+
+If you can not make our weekday PRIME hours, you are more than welcome to join our weekend PRIME hours! Other than that, we have no options available to earn prime points.
+
+### I started a game a few minutes before PRIME hours started, but I didn’t receive any points?
+
+Points are given to matches that start during prime hours, not end. Make sure you wait until PRIME hours begin to start your matches!
+
+### I play on multiple accounts, can my points add up to total one amount?
+
+All points earned will be applied to each respective IGN that the games are played on, there is no additive format that allows a stacking of accounts.
+
+### I’ve played multiple games during PRIME hours, but I don’t see my points updating? What is going on?
+
+If your points do not show up within 30 minutes then please contact us at twitter.com/vgprogg
+
+### Is there any difference between solo queue and team queue?
+
+You will earn the same amount of points in both solo queue and team queue.
+
+### My match finished after VGPRIME ended, does it count still?
+
+You are safe, as long as you started you match prior the VGPRIME slot time ended.
+
+### How do I get MVP?
+
+By performing the best within players in the match. This MVP award will follow the same MVP algorithm on vgpro.gg
+
+### Can I play any mode to earn points?
+
+Yes! We recommend visiting our Format section to learn more about how our points system works for each respective game mode.
+
+### How many people am I competing against?
+
+You are competing against players that choose to play during our prime hours.
+
+### I lost but still want to get points, how do I do it?
+
+You will points win or lose, but you will earn a bit more from wins and MVP awards.
+
+### I am so bad, how do I win?
+
+Skill is not a major factor in earning points. Play during PRIME hours to earn points!
+
+### I don't know how to play, how do I win?
+
+We recommend learning while you play. PRIME hours allow for points to be earned based off of time played, not all skill!
+
+### I am busy during prime time, what do I do?
+
+If you can not make hours during the weekday, we recommend looking at our hours during the weekend for the separate weekend events!
+`;
---
+++ components/md/rulesContent.ts
@@ -0,0 +1,70 @@
+export default `
+# Rules and Regulations
+
+> VGPRO reserves the right to modify rules as needed, without notice.
+
+**All rules are enforced as of 12/01/2018**
+
+1.  By participating in VGPRIME, participant fully agrees to all of the rules without reserve.
+
+2.  Entry to VGPRIME determined by having an active vgpro.gg IGN.
+
+3.  Participation to VGPRIME officially begins after ONE (1) matches are played. The ONE (1) match will count towards your points respectively as mentioned in the Format section.
+
+4.  Matches played before or after VGPRIME hours will NOT count towards VGPRIME points. See more in Format section below.
+
+5.  Points are earned and applied under the IGN the games were played on.
+
+6.  VGPRIME Points are distributed as stated in Format section
+
+7.  Points will be void if participant is found in the following:
+
+    - Win Trading
+    - Intentional Feeding
+    - Account Sharing
+    - Being found in violation of http://forums.vainglorygame.com/index.php?help/terms
+
+8.  Any participant caught in violation of rules will result in disqualification from VGPRIME totaling 3 months time.
+
+9.  Monthly prize distribution will be at the sole discretion and release of VGPRO.gg
+
+10. Weekend prize distribution will be at the sole discretion and release of VGPRO.gg
+
+11. Any reports of misconduct are to be reported at contact@vgpro.gg
+
+12. Misuse and tampering of screenshots used in report system will result in disqualification from VGPRIME totaling 3 months time.
+
+### Format
+
+#### VGPRIME HOURS
+
+| Region          | Start | End | Timezone          |
+|-----------------|-------|-----|-------------------|
+| North America   | 5PM   | 8PM |  PST              |
+| Europe          | 5PM   | 8PM |  GMT+1            |
+| South America   | 6PM   | 9PM |  Brazil UTC-02:00 |
+| South East Asia | 6PM   | 9PM |                   |
+| East Asia       | 6PM   | 9PM |  Korea UTC+09:00  |
+| China           | 6PM   | 9PM |  UTC+08:00        |
+
+#### Points System
+
+| Game Mode     | Win | Loss | MVP Bonus |
+|---------------|-----|------|-----------|
+| Ranked 5V5    | 34  | 17   | 8         |
+| Casual 5V5    | 27  | 16   | 8         |
+| Ranked 3V3    | 32  | 13   | 6         |
+| Casual 3V3    | 24  | 12   | 6         |
+| Battle Royale | 12  | 3    | 3         |
+| Blitz         | 7   | 6    | 1         |
+
+#### Prizes
+
+| Final Position | Monthly Leaderboard | Weekly Leaderboard |
+|----------------|---------------------|--------------------|
+| Top 1          | $300                | $50                |
+| Top 2          | $150                | $30                |
+| Top 3          | $100                | $20                |
+| Top 4          | $50                 |                    |
+| Top 5-15       | $10                 |                    |
+`;
