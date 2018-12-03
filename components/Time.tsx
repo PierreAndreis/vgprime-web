d86370b43a5bcba6994308c98f885d55c2da3941
Pierre Ortega <pierreandreisortega@gmail.com>
2018-12-03 00:02:28 -0800

    remove/ unused svg

--- components/Time.tsx
+++ components/Time.tsx
@@ -153,11 +153,11 @@ export default class extends React.Component<{}> {
       <div className={container}>
         <h2>
           PRIME HOURS{" "}
-          <span>
+          {/* <span>
             <svg width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M11,17H13V11H11V17Z" />
             </svg>
-          </span>
+          </span> */}
         </h2>
         <Query query={qPrimeHours}>
           {({ data, loading }) => {
